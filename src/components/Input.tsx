89f1cacdb5903e0bc28c37518a8dcd053c3fdd7d
Julius Rajala <juliusrajala@gmail.com>
2020-04-09 19:45:25 +0300

    Built input component.

---
+++ src/components/Input.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  label: string;
+  inputProps?: React.HTMLProps<HTMLInputElement>;
+  onChange: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Component = (props: Props) => {
+  return (
+    <Container>
+      <Label>{props.label}</Label>
+      <Input onChange={props.onChange} {...props.inputProps} />
+    </Container>
+  );
+};
+
+export default Component;
+
+const Container = styled.label`
+  display: flex;
+  max-width: 300px;
+  flex-direction: column;
+`;
+
+// There's a better typing to this that I can't bet bothered to set together
+// it needs to be used to allow for setting the inputProps.
+const Input: any = styled.input`
+  background: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+`;
+
+const Label = styled.span`
+  font-weight: 600;
+  font-size: 1rem;
+  margin-bottom: 0.25rem;
+`;
--- src/components/NewUserForm.tsx
+++ src/components/NewUserForm.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { postNewUser } from 'src/core/api';
+import Input from 'src/components/Input';
+import { FiUnlock } from 'react-icons/fi';
 
 const NewUserForm = () => {
   const [credentials, setCredentials] = React.useState({ name: '', email: '' });
@@ -11,25 +13,25 @@ const NewUserForm = () => {
 
   return (
     <UserForm>
-      <div>
-        <label>
-          <span>Name</span>
-          <input
-            onChange={(ev) =>
-              setCredentials({ ...credentials, name: ev.target.value })
-            }
-          />
-        </label>
-        <label>
-          <span>Email</span>
-          <input
-            onChange={(ev) =>
-              setCredentials({ ...credentials, email: ev.target.value })
-            }
-          />
-        </label>
-      </div>
-      <button>Login</button>
+      <Input
+        label="Name"
+        onChange={(ev) =>
+          setCredentials({ ...credentials, name: ev.target.value })
+        }
+        inputProps={{
+          autoFocus: true,
+        }}
+      />
+      <Input
+        label="Email"
+        onChange={(ev) =>
+          setCredentials({ ...credentials, email: ev.target.value })
+        }
+      />
+      <Button role="button">
+        <FiUnlock />
+        Login
+      </Button>
     </UserForm>
   );
 };
@@ -39,4 +41,31 @@ export default NewUserForm;
 const UserForm = styled.form`
   display: flex;
   flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: #bb3f3f;
+  border: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  cursor: pointer;
+  padding: 0.5rem;
+  margin: 0.25rem;
+  color: #fff;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 150px;
+  margin-top: 1rem;
+  font-size: 1rem;
+
+  svg {
+    margin-right: 0.5rem;
+  }
+
+  &:hover,
+  &:focus {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
 `;
