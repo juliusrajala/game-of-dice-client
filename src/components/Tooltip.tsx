014e64e86b3fa73e7d56abe2e388fd0554370839
Julius Rajala <juliusrajala@gmail.com>
2020-04-15 22:23:33 +0300

    Built in tooltip-component.

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  label: string;
+  children: React.ReactChildren;
+}
+
+const Tooltip = (props: Props) => {
+  const [showTooltip, setShowTooltip] = React.useState(false);
+  return (
+    <Container
+      onMouseEnter={() => !showTooltip && setShowTooltip(true)}
+      onMouseLeave={() => showTooltip && setShowTooltip(false)}
+    >
+      {showTooltip && <Tip>{props.label}</Tip>}
+      {props.children}
+    </Container>
+  );
+};
+
+export default Tooltip;
+
+const Container = styled.div`
+  margin: none;
+  padding: none;
+  height: auto;
+  width: auto;
+  display: flex;
+  position: relative;
+`;
+
+const Tip = styled.div`
+  padding: 0.5rem;
+  background: #fff;
+  color: black;
+  position: absolute;
+`;
